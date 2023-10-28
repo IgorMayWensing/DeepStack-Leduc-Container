f9624d5353ab8f729670f8cce95061c1b93f1b8d
Igor May Wensing <igormaywensing@gmail.com>
2023-10-28 19:50:54 -0300

    add base ai_agent_api, blueprint and minor fixes

--- ACPCServer/tcc_ai_player.c
+++ ACPCServer/tcc_ai_player.c
@@ -16,6 +16,113 @@ Copyright (C) 2011 by the Computer Poker Research Group, University of Alberta
 #include "game.h"
 #include "rng.h"
 #include "net.h"
+#include <json-c/json.h>
+
+// Function to convert card rank to its character representation
+char rankToChar(uint8_t rank) {
+    switch (rank) {
+        case 0: return 'Q';
+        case 1: return 'K';
+        case 2: return 'A';
+        default: return '?'; // Error case, should not happen
+    }
+}
+
+// infostate_translator() is a function that takes in a MatchState and a Game and returns a string representation of the infostate
+char* infostate_translator(MatchState state, Game game) {
+    static char infostate[100]; // Static to ensure memory remains allocated after function returns
+
+    // Extract player's hole cards
+    char holeCards[MAX_HOLE_CARDS + 1];
+    for (int i = 0; i < game.numHoleCards; i++) {
+        holeCards[i] = rankToChar(rankOfCard(state.state.holeCards[state.viewingPlayer][i]));
+    }
+    holeCards[game.numHoleCards] = '\0';
+
+    // Extract board cards (if they exist)
+    char boardCards[MAX_BOARD_CARDS + 1] = "";
+    if (state.state.round > 0) { // If we're past the pre-flop round
+        for (int i = 0; i < game.numBoardCards[0]; i++) { // Assuming flop is the first set of board cards
+            boardCards[i] = rankToChar(rankOfCard(state.state.boardCards[i]));
+        }
+        boardCards[game.numBoardCards[0]] = '\0';
+    }
+
+    // Extract action history
+    char actionHistory[MAX_NUM_ACTIONS + 1];
+    int actionCount = 0;
+    for (int i = 0; i < state.state.numActions[0]; i++) { // For pre-flop actions
+        Action act = state.state.action[0][i];
+        if (act.type == a_fold || act.type == a_call) {
+            actionHistory[actionCount++] = 'p';
+        } else if (act.type == a_raise) {
+            int raiseAmount = act.size / 100; // Assuming raise sizes are in multiples of 100
+            actionHistory[actionCount++] = (raiseAmount <= 9) ? '1' + raiseAmount - 1 : 'a' + raiseAmount - 10;
+        }
+    }
+    if (actionCount % 2 == 1) {
+        actionHistory[actionCount++] = 'p';
+    }
+    actionHistory[actionCount++] = '/';
+
+    for (int i = 0; i < state.state.numActions[1]; i++) { // For flop actions
+        Action act = state.state.action[1][i];
+        if (act.type == a_fold || act.type == a_call) {
+            actionHistory[actionCount++] = 'p';
+        } else if (act.type == a_raise) {
+            int raiseAmount = act.size / 100;
+            actionHistory[actionCount++] = (raiseAmount <= 9) ? '1' + raiseAmount - 1 : 'a' + raiseAmount - 10;
+        }
+    }
+    actionHistory[actionCount] = '\0';
+
+    // Combine all parts to form the infostate string
+    sprintf(infostate, "%s%s%s", holeCards, boardCards, actionHistory);
+
+    return infostate;
+}
+
+json_object* loadJSON(const char* filename) {
+    FILE *f = fopen(filename, "r");
+    if (!f) {
+        perror("Failed to open the file");
+        exit(EXIT_FAILURE);
+    }
+
+    fseek(f, 0, SEEK_END);
+    long fsize = ftell(f);
+    fseek(f, 0, SEEK_SET);
+
+    char *string = malloc(fsize + 1);
+    fread(string, 1, fsize, f);
+    fclose(f);
+    string[fsize] = 0;
+
+    json_object *jobj = json_tokener_parse(string);
+    free(string);
+    return jobj;
+}
+
+void getActionsAndProbs(json_object *strategy, const char *infostate, int **actions, double **probs, int *num_actions) {
+    json_object *entry;
+    if (json_object_object_get_ex(strategy, infostate, &entry)) {
+        json_object *actions_obj = json_object_array_get_idx(entry, 0);
+        json_object *probs_obj = json_object_array_get_idx(entry, 1);
+        
+        *num_actions = json_object_array_length(actions_obj);
+        *actions = malloc(*num_actions * sizeof(int));
+        *probs = malloc(*num_actions * sizeof(double));
+
+        for (int i = 0; i < *num_actions; i++) {
+            (*actions)[i] = json_object_get_int(json_object_array_get_idx(actions_obj, i));
+            (*probs)[i] = json_object_get_double(json_object_array_get_idx(probs_obj, i));
+        }
+    } else {
+        *num_actions = 0;
+        *actions = NULL;
+        *probs = NULL;
+    }
+}
 
 int main( int argc, char **argv )
 {
@@ -40,6 +147,12 @@ int main( int argc, char **argv )
 
     fprintf( stderr, "usage: player game server port\n" );
     exit( EXIT_FAILURE );
+  }
+
+  json_object *strategy = loadJSON("../blueprints/hjO-mccfr-6cards-11maxbet-EPcfr0-mRW0_0-iter10000.json");
+  if (strategy == NULL) {
+    fprintf(stderr, "ERROR: could not load JSON file\n");
+    exit(EXIT_FAILURE);
   }
 
   /* Define the probabilities of actions for the player */
@@ -125,56 +238,38 @@ int main( int argc, char **argv )
     line[ len ] = ':';
     ++len;
 
-    /* build the set of valid actions */
-    p = 0;
-    for( a = 0; a < NUM_ACTION_TYPES; ++a ) {
+    char infostate[100];
 
-      actionProbs[ a ] = 0.0;
-    }
+    infostate = infostate_translator(state, game); //infostate_translator needs to be implemented
 
-    /* consider fold */
-    action.type = a_fold;
-    action.size = 0;
-    if( isValidAction( game, &state.state, 0, &action ) ) {
+    sprintf(infostate, "%s", "YOUR_INFOSTATE_REPRESENTATION_HERE");
 
-      actionProbs[ a_fold ] = probs[ a_fold ];
-      p += probs[ a_fold ];
-    }
+    int *possible_actions;
+    double *action_probs;
+    int num_possible_actions;
 
-    /* consider call */
-    action.type = a_call;
-    action.size = 0;
-    actionProbs[ a_call ] = probs[ a_call ];
-    p += probs[ a_call ];
+    getActionsAndProbs(strategy, infostate, &possible_actions, &action_probs, &num_possible_actions);
 
-    /* consider raise */
-    if( raiseIsValid( game, &state.state, &min, &max ) ) {
+    if (num_possible_actions > 0) {
+        double p = genrand_real2(&rng);
+        int chosen_action_index = 0;
+        for (int i = 0; i < num_possible_actions; i++) {
+            if (p <= action_probs[i]) {
+                chosen_action_index = i;
+                break;
+            }
+            p -= action_probs[i];
+        }
 
-      actionProbs[ a_raise ] = probs[ a_raise ];
-      p += probs[ a_raise ];
-    }
+        action.type = (enum ActionType)possible_actions[chosen_action_index];
+        // If your action type requires a size (like a raise amount), you'll need to set action.size here
 
-    /* normalise the probabilities  */
-    assert( p > 0.0 );
-    for( a = 0; a < NUM_ACTION_TYPES; ++a ) {
-
-      actionProbs[ a ] /= p;
-    }
-
-    /* choose one of the valid actions at random */
-    p = genrand_real2( &rng );
-    for( a = 0; a < NUM_ACTION_TYPES - 1; ++a ) {
-
-      if( p <= actionProbs[ a ] ) {
-
-        break;
-      }
-      p -= actionProbs[ a ];
-    }
-    action.type = (enum ActionType)a;
-    if( a == a_raise ) {
-
-      action.size = min + genrand_int32( &rng ) % ( max - min + 1 );
+        free(possible_actions);
+        free(action_probs);
+    } else {
+        // Handle the case where the infostate is not found in the strategy
+        action.type = a_fold;  // Default to fold
+        action.size = 0;
     }
 
     /* do the action! */
@@ -200,5 +295,6 @@ int main( int argc, char **argv )
     fflush( toServer );
   }
 
+  json_object_put(strategy);
   return EXIT_SUCCESS;
 }
--- Dockerfile
+++ Dockerfile
@@ -9,6 +9,7 @@ RUN apt-get update && apt-get install -y \
     python3 \
     sudo \
     nano \
+    libjson-c-dev \
  && apt-get autoremove -y && apt-get clean && rm -rf /var/lib/apt/lists/*
 
 # Clone, modify and install Torch
