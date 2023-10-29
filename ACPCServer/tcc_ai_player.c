790bd432bcdfc13cfb56e5104183d58421f878e1
Igor May Wensing <igormaywensing@gmail.com>
2023-10-29 10:36:46 -0300

    asdasd

--- ACPCServer/tcc_ai_player.c
+++ ACPCServer/tcc_ai_player.c
@@ -28,7 +28,9 @@ char* infostate_translator(const Game *game, const State *state) {
     printf("maxSpent: %u\n", state->maxSpent);
     printf("minNoLimitRaiseTo: %u\n", state->minNoLimitRaiseTo);
     printf("0 holeCards[][]: %u\n", state->holeCards[0][0]); //type is uint8_t
+    printf("rankOfCard 0 holeCards[][]: %u\n", rankOfCard(state->holeCards[0][0])); //type is uint8_t
     printf("1 holeCards[][]: %u\n", state->holeCards[1][0]); //type is uint8_t
+    printf("rankOfCard 1 holeCards[][]: %u\n", rankOfCard(state->holeCards[1][0])); //type is uint8_t
     printf("boardCards[][]: %u\n\n", state->boardCards[0]); //type is uint8_t
     
     // You can add more prints to detail out game and state
