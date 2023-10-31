7e10c93f6dcf1db6af22fa5ed3c15a1c78681709
Igor May Wensing <igormaywensing@gmail.com>
2023-10-31 00:51:17 -0300

    fix min raise

--- ACPCServer/matchstate_to_action.py
+++ ACPCServer/matchstate_to_action.py
@@ -127,7 +127,7 @@ def action_from_code(action_code, selected_action_index, last_action):
     if selected_action_index in dict:
         action = dict[selected_action_index]
     else:
-        action = "r" + str(action_code) + "00"
+        action = "r" + str(min(action_code+1, 12)) + "00"
     return action
 
 #r600:|A
